be2ad73851ab78e68fcc6e34eddbee15b51fd7a5
Akshay Khole <akshay@reversehack.in>
2015-07-01 12:36:03 -0700

    You are here on the map

--- Lazy Maps/ViewController.swift
+++ Lazy Maps/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+// 37.312926, -121.975142
 
+class ViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var latitude:CLLocationDegrees = 37.312926
+        var longtidue:CLLocationDegrees = -121.975142
+        
+        var latDelta:CLLocationDegrees = 0.01
+        var lonDelta:CLLocationDegrees = 0.01
+        
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        var location:CLLocationCoordinate2D =
+        CLLocationCoordinate2DMake(latitude, longtidue)
+        
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        map.setRegion(region, animated: true)
+        
+        // add annotation
+        
+        var annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "You are here"
+        map.addAnnotation(annotation)
+        map.selectAnnotation(annotation, animated: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
